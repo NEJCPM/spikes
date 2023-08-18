03991259e8f31226ee32fd798887612ce1b6512f
William Yucciolino <william.yucciolino@jcpm.com.br>
2023-08-18 16:39:47 -0300

    spike rust

---
+++ low_latency_config_api/rust/src/main.rs
@@ -0,0 +1,42 @@
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
+fn main() {
+    let listener = TcpListener::bind("0.0.0.0:3003").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let _http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = "<!DOCTYPE html>
+    <html lang=\"en\">
+      <head>
+        <meta charset=\"utf-8\" />
+        <title>Hello!</title>
+      </head>
+      <body>
+        <h1>Hello!</h1>
+        <p>Hi from Rust</p>
+      </body>
+    </html>";
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
+}
